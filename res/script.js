0dc78c932195996389c5df61be9dc69f4b7f0e5e
Marco Andronaco <andronacomarco@gmail.com>
2022-09-18 19:00:20 +0200

    add sfx

--- res/script.js
+++ res/script.js
@@ -4,6 +4,8 @@ const source = document.getElementById("music-source");
 const music_base = document.getElementById("music-player-base");
 const music_song = document.getElementById("music-player-song");
 const music_song_text = document.getElementById("song-text");
+const clickAudio1 = new Audio(url="res/sfx/os/click2.ogg");
+const clickAudio2 = new Audio(url="res/sfx/os/click1.ogg");
 
 const tracks = {
     "awesome": {
@@ -54,14 +56,31 @@ const page_tracks = {
     "zelda": "forest",
 }
 
+let first = false;
 let play = false;
 let currentPage = "about";
 let currentTrack = "";
-let timeout;
+let showTimeout, hideTimeout;
+
+function playAudio(audio){
+    audio.volume = 0.1;
+    audio.currentTime = 0;
+    audio.play();
+}
 
 function firstClickPlay(element) {
+    click1 = clickAudio1 || parent.clickAudio1;
+    click2 = clickAudio2 || parent.clickAudio2;
+
+    playAudio(click1)
+    element.onmousedown = () => playAudio(click1);
+    element.onmouseup = () => playAudio(click2);
+
+    if(first) return;
+    document.getElementById("music-player").style.bottom = "0";
+    music.volume = 0.1;
     playPause(false, page_tracks[currentPage]);
-    element.onclick = null;
+    first = true;
 }
 
 function goToPage(page) {
@@ -78,28 +97,39 @@ function formatTrack(track) {
 
 function showTitle(){
     if (!currentTrack) return;
-    clearTimeout(timeout);
-    music_song_text.textContent = formatTrack(tracks[currentTrack]);
-    music_song.style.top = "0";
-    timeout = setTimeout(() => { music_song.style.top = "100%"; }, 3_000);
+    hideTitle();
+    clearTimeout(showTimeout);
+    clearTimeout(hideTimeout);
+    
+    showTimeout = setTimeout(() => { 
+        music_song_text.textContent = formatTrack(tracks[currentTrack]);
+        music_song.style.top = "0";
+    }, 400);
+
+    hideTimeout = setTimeout(() => hideTitle(), 3_400);
+}
+
+function hideTitle(){
+    music_song.style.top = "100%";
 }
 
 function playPause(toggle=play, track=currentTrack) {
+
     if (toggle){
         music_base.classList.remove("player-play");
         music.pause();
         play = false
+        hideTitle();
     }
     else {
         music_base.classList.add("player-play");
-        music.volume = 0.1;
         if (track != currentTrack) {
             currentTrack = track;
             source.src = "res/mus/" + tracks[currentTrack].file_name;
             music.load();
         }
-        showTitle();
         music.play().catch(() => {});
+        showTitle();
         play = true
     }
 }
