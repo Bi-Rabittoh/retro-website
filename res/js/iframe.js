8362008c2a90a430cf4d088b3f7960b85b6bc3db
Marco Andronaco <andronacomarco@gmail.com>
2022-09-20 15:17:11 +0200

    add plain version and create gf page

--- res/js/iframe.js
+++ res/js/iframe.js
@@ -1,1 +1,19 @@
-hide_all();
+
+if (!parent.location.href.includes("plain")) {
+    hide_all();
+}
+const page_height = {
+    "about": 1500,
+    "anime": 1000,
+    "dragonquest": 1000,
+    "earthbound": 1000,
+    "gf": 1600,
+    "indies": 1000,
+    "zelda": 1000
+}
+
+const page = document.body.getAttribute("data-page");
+const iframe = parent.document.getElementById("mainframe");
+
+iframe.height = page_height[page];
+iframe.scrolling = "no";
